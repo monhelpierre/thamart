c8089a87af6d18148a62f1f91da1715d5daa0035
Monhel Maudoony Pierre <monhelmaudoonypierre@gmail.com>
2026-06-11 20:20:23 -0300

    Hidrate error solved

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,9 +10,14 @@ export const metadata: Metadata = {
   description: "Artesanato em miçanga – pulseiras, colares e brincos",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="pt">
+    <html lang="pt" suppressHydrationWarning>
+      <head />
       <body className={inter.className}>
         <I18nProvider>{children}</I18nProvider>
       </body>
